c2551f5eff862b2a81be21cd4ac9362bed881b5c
soumyakanti <24433698+Samthesedate@users.noreply.github.com>
2021-02-08 18:53:57 +0530

    No idea

--- chapter1.py
+++ chapter1.py
@@ -1,3 +1,10 @@
 import cv2
 
-print("Lolol")
+cap = cv2.VideoCapture(3)
+cap.set(3, 640)
+cap.set(4, 480)
+while True:
+    sucess, img = cap.read()
+    cv2.imshow("video", img)
+    if cv2.waitKey(int(1000 / 30)) & 0xFF == ord("q"):
+        break
